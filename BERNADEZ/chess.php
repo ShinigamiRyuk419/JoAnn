67579da7f62cce1dffcc351862384a9f9ecfb3ad
ShinigamiRyuk419 <112138312+ShinigamiRyuk419@users.noreply.github.com>
2022-09-23 20:09:01 -0700

    Update chess.php

--- BERNADEZ/chess.php
+++ BERNADEZ/chess.php
@@ -145,6 +145,15 @@ krsort() - sort associative arrays in descending order, according to the key -->
 	    }	
     }
     ?>
+	  <?php
+    function sampleFunction ($num1, $num2){
+        
+     return ($num1 == 30) || ($num2 == 30) || ($num1 + $num2 ==30 );
+    }
+
+echo var_dump(sampleFunction(15,13)) ;
+echo var_dump(sampleFunction(15,15)) ;
+echo var_dump(sampleFunction(0,30)) ;
   </section>
   </body>
   </html>
